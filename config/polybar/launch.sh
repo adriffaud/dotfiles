eff975df402c7ad4d69d4dca314ce8a0ba6858f2
Adrien Driffaud <adriffaud@sierrawireless.com>
2019-05-10 12:39:55 +0200

    Polybar: switch to single bar

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -10,6 +10,5 @@ while pgrep -u adriffaud -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-	  WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload default -c ~/.config/polybar/config.ini &
-	  WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload bottom -c ~/.config/polybar/config.ini &
+	  WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload default -c ~/.config/polybar/config &
 done
