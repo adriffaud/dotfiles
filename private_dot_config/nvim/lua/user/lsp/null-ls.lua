df5f109ed42f69120f11e5aa09c3db8061ac668b
Adrien Driffaud <adrien@driffaud.fr>
2022-09-20 11:34:12 +0200

    Setup null-ls format on save

--- private_dot_config/nvim/lua/user/lsp/null-ls.lua
+++ private_dot_config/nvim/lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -8,24 +8,38 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
 -- npm install --save-dev prettier prettier-plugin-solidity
-null_ls.setup {
-  debug = false,
-  sources = {
-    formatting.prettier.with {
-      extra_filetypes = { "toml", "solidity" },
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-    },
-    formatting.black.with { extra_args = { "--fast" } },
-    formatting.stylua,
-    formatting.shfmt,
-    formatting.mix,
-    diagnostics.credo,
-    diagnostics.flake8,
-    diagnostics.shellcheck,
-  },
-}
+null_ls.setup({
+	debug = false,
+	sources = {
+		formatting.prettier.with({
+			extra_filetypes = { "toml", "solidity" },
+			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+		}),
+		formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.stylua,
+		formatting.shfmt,
+		formatting.mix,
+		diagnostics.credo,
+		diagnostics.flake8,
+		diagnostics.shellcheck,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.formatting({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
+})
 
 -- local unwrap = {
 --   method = null_ls.methods.DIAGNOSTICS,
