8aaee459996c0fa34898782ba3039e210028024c
Adrien Driffaud <adrien@driffaud.fr>
2022-12-16 09:47:04 +0100

    Update nvim config

--- private_dot_config/nvim/lua/user/init.lua
+++ private_dot_config/nvim/lua/user/init.lua
@@ -1,26 +1,17 @@
 local config = {
   updater = { channel = "stable" },
 
-  colorscheme = "gruvbox",
+  colorscheme = "tokyonight-storm",
 
-  highlights = {
-    gruvbox = {
-      StatusLine = { fg = "#ebdbb2", bg = "#504945" }
+  highlights = {},
+
+  options = {
+    opt = {
+      background = "dark"
     }
   },
 
   lsp = {
-    on_attach = function(_, bufnr)
-      local diagnostics_active = true
-      vim.keymap.set("n", "<leader>lt", function()
-        diagnostics_active = not diagnostics_active
-        if diagnostics_active then
-          vim.diagnostic.show()
-        else
-          vim.diagnostic.hide()
-        end
-      end, { buffer = bufnr, desc = "Toggle diagnostics" })
-    end,
     yamlls = {
       settings = {
         yaml = {
@@ -34,46 +25,10 @@ local config = {
 
   plugins = {
     init = {
-      ["ellisonleao/gruvbox.nvim"] = {
-        config = function()
-          require("gruvbox").setup {
-            italic = false
-          }
-        end
-      },
-      ["nvim-neorg/neorg"] = {
-        config = function()
-          require("neorg").setup {
-            load = {
-              ["core.defaults"] = {},
-              ["core.norg.concealer"] = {},
-              ["core.norg.completion"] = {
-                config = {
-                  engine = "nvim-cmp",
-                },
-              },
-              ["core.norg.dirman"] = {
-                config = {
-                  workspaces = {
-                    work = "~/notes/work",
-                    home = "~/notes/home",
-                    brain = "~/notes/brain",
-                    gtd = "~/notes/brain/gtd",
-                  },
-                },
-              },
-              ["core.gtd.base"] = {
-                config = {
-                  workspace = "gtd",
-                },
-              },
-            },
-          }
-        end,
-        requires = {
-          { "nvim-lua/plenary.nvim" },
-        },
-      },
+      ["akinsho/bufferline.nvim"] = { disable = true },
+      ["stevearc/aerial.nvim"] = { disable = true },
+      { "folke/tokyonight.nvim" },
+      { "p00f/alabaster.nvim" }
     },
   },
 }
