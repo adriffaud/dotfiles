8720d40029c2ca9f2597fb26c348422b76eee6ff
Adrien Driffaud <adrien@driffaud.fr>
2023-02-14 09:01:21 +0100

    Update nvim config

--- private_dot_config/nvim/lua/user/init.lua
+++ private_dot_config/nvim/lua/user/init.lua
@@ -1,36 +1,32 @@
 local config = {
-  updater = { channel = "stable" },
-
-  colorscheme = "catppuccin-frappe",
-
-  highlights = {},
-
-  options = {
-    opt = {
-      background = "dark"
-    }
-  },
-
-  lsp = {
-    yamlls = {
-      settings = {
-        yaml = {
-          schemas = {
-            ["https://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}"
-          }
+    updater = { channel = "stable" },
+    colorscheme = "catppuccin-frappe",
+    highlights = {},
+    options = {
+        opt = {
+            background = "dark"
         }
-      }
-    }
-  },
-
-  plugins = {
-    init = {
-      ["akinsho/bufferline.nvim"] = { disable = true },
-      ["stevearc/aerial.nvim"] = { disable = true },
-      { "folke/tokyonight.nvim" },
-      { "catppuccin/nvim" }
     },
-  },
+    lsp = {
+        yamlls = {
+            settings = {
+                yaml = {
+                    schemas = {
+                        ["https://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}"
+                    }
+                }
+            }
+        }
+    },
+    plugins = {
+        init = {
+            ["akinsho/bufferline.nvim"] = { disable = true },
+            ["stevearc/aerial.nvim"] = { disable = true },
+            { "folke/tokyonight.nvim" },
+            { "catppuccin/nvim" },
+            { "cuducos/yaml.nvim" }
+        },
+    },
 }
 
 return config
