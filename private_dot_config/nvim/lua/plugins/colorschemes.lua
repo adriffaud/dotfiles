df2cb86fdaefb9b18c78536a7fb321a9a25b23ab
Adrien Driffaud <adrien@driffaud.fr>
2024-02-08 18:14:25 +0100

    nvim config cleanup

--- private_dot_config/nvim/lua/plugins/colorschemes.lua
+++
@@ -1,5 +0,0 @@
-return {
-  { "rose-pine/neovim", name = "rose-pine" },
-  { "catppuccin/nvim", name = "catppuccin" },
-  -- { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin-frappe" } },
-}
--- private_dot_config/nvim/lua/plugins/extra.lua
+++ private_dot_config/nvim/lua/plugins/extra.lua
@@ -1,10 +1,15 @@
 return {
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+
   {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
       "vrischmann/tree-sitter-templ",
       config = function()
         local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
         treesitter_parser_config["templ"] = {
           filetype = "templ",
           maintainers = { "@vrischmann" },
