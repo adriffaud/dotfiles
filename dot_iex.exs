10886fa5946b037adbf0aba1b400e7e60e96f36f
Adrien Driffaud <adrien@driffaud.fr>
2024-02-08 18:13:20 +0100

    tools tweaks

--- dot_iex.exs
+++ dot_iex.exs
@@ -4,25 +4,3 @@ import_if_available(Ecto.Changeset)
 Mix.ensure_application!(:wx)
 Mix.ensure_application!(:runtime_tools)
 Mix.ensure_application!(:observer)
-
-Application.put_env(:elixir, :ansi_enabled, true)
-
-IEx.configure(
-  colors: [
-    enabled: true,
-    eval_result: [:green, :bright],
-    eval_error: [:red, :bright],
-    eval_info: [:yellow, :bright]
-  ],
-  default_prompt:
-    [
-      "\e[G",
-      "🧪 ",
-      :magenta,
-      "%prefix",
-      " ▶",
-      :reset
-    ]
-    |> IO.ANSI.format()
-    |> IO.chardata_to_string()
-)
