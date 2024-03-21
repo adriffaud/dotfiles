ac078671aae9f4983c4e85b862c5a6e33a2de206
Adrien Driffaud <adrien@driffaud.fr>
2024-03-21 12:01:07 +0100

    add wezterm config

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,51 @@
+local wezterm = require("wezterm")
+local mux = wezterm.mux
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+local function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "tokyonight-storm"
+	else
+		return "Builtin Solarized Light"
+	end
+end
+
+wezterm.on("gui-startup", function(cmd)
+	local _, _, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
+wezterm.on("gui-attached", function(_)
+	local workspace = mux.get_active_workspace()
+	for _, window in ipairs(mux.all_windows()) do
+		if window:get_workspace() == workspace then
+			window:gui_window():maximize()
+		end
+	end
+end)
+
+config.color_scheme = scheme_for_appearance(get_appearance())
+config.enable_wayland = false
+config.font_size = 11.0
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+return config
